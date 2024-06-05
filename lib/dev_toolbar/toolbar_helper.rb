73df5436a0bc5ebc17189eed7570945337a6d805
Ben Purinton <ben.purinton@gmail.com>
2024-06-05 12:33:55 -0700

    fix css

--- lib/dev_toolbar/toolbar_helper.rb
+++ lib/dev_toolbar/toolbar_helper.rb
@@ -4,10 +4,19 @@ module DevToolbar
       return unless Rails.env.development?
 
       links = DevToolbar.configuration.links.map do |link|
-        link_to(link[:name], link[:path], style: 'color: #fff; margin-right: 2rem;')
+        link_to(
+          link[:name], 
+          link[:path], 
+          style: "color: #fff; margin-right: 0.5rem;"
+        )
       end.join.html_safe
 
-      content_tag(:div, links, id: 'dev-toolbar', style: 'position: fixed; right: 0; top: 0; background: #333; color: #fff; padding: 2rem; z-index: 1000;')
+      content_tag(
+        :div, 
+        links, 
+        id: "dev-toolbar", 
+        style: "position: fixed; right: 0; top: 0; background: #333; color: #fff; padding: 0.5rem; z-index: 1000;"
+      )
     end
   end
 end
