1fdb09e8bd7adc1da7abfa5b3cab11621eac5619
Ben Purinton <ben.purinton@gmail.com>
2024-06-05 10:12:08 -0700

    use relative file paths in railtie

--- lib/dev_toolbar/railtie.rb
+++ lib/dev_toolbar/railtie.rb
@@ -8,8 +8,8 @@ module DevToolbar
 
     initializer "dev_toolbar.assets" do |app|
       app.config.assets.precompile += %w(dev_toolbar.css dev_toolbar.js)
-      app.config.assets.paths << root.join("app", "assets", "stylesheets")
-      app.config.assets.paths << root.join("app", "assets", "javascripts")
+      app.config.assets.paths << File.expand_path('../../app/assets/stylesheets', __FILE__)
+      app.config.assets.paths << File.expand_path('../../app/assets/javascripts', __FILE__)
     end
   end
 end
