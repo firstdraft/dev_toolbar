d309817c0c090304634f4a9382c050c8611357ea
Ben Purinton <ben.purinton@gmail.com>
2024-06-05 12:35:13 -0700

    open in new tab

--- lib/dev_toolbar/toolbar_helper.rb
+++ lib/dev_toolbar/toolbar_helper.rb
@@ -6,8 +6,9 @@ module DevToolbar
       links = DevToolbar.configuration.links.map do |link|
         link_to(
           link[:name], 
-          link[:path], 
-          style: "color: #fff; margin-right: 0.5rem;"
+          link[:path],
+          style: "color: #fff; margin-right: 0.5rem;",
+          target: "_blank",
         )
       end.join.html_safe
 
@@ -15,7 +16,7 @@ module DevToolbar
         :div, 
         links, 
         id: "dev-toolbar", 
-        style: "position: fixed; right: 0; top: 0; background: #333; color: #fff; padding: 0.5rem; z-index: 1000;"
+        style: "position: fixed; right: 0; top: 0; background: #333; color: #fff; padding: 0.5rem; z-index: 1000;",
       )
     end
   end
