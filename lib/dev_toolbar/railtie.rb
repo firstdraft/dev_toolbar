7e6db0dc273b2cbe641d5303d98fc018643a71d8
Ben Purinton <ben.purinton@gmail.com>
2024-06-05 11:57:41 -0700

    fix railtie

--- lib/dev_toolbar/railtie.rb
+++ lib/dev_toolbar/railtie.rb
@@ -2,7 +2,7 @@ module DevToolbar
   class Railtie < Rails::Railtie
     initializer "dev_toolbar.configure_view_controller" do
       ActiveSupport.on_load(:action_controller) do
-        include DevToolbar::ToolbarHelper
+        ActionController::Base.helper DevToolbar::ToolbarHelper
       end
     end
 
