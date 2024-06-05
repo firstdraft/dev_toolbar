d69b7c090667fcfb4ffa354241bc4cea3741235b
Ben Purinton <ben.purinton@gmail.com>
2024-06-05 11:53:00 -0700

    add configuration

--- lib/dev_toolbar.rb
+++ lib/dev_toolbar.rb
@@ -3,8 +3,17 @@
 require_relative "dev_toolbar/version"
 require_relative "dev_toolbar/railtie"
 require_relative "dev_toolbar/toolbar_helper"
+require "dev_toolbar/configuration"
 
 module DevToolbar
   class Error < StandardError; end
-  # Your code goes here...
+  
+  class << self
+    attr_accessor :configuration
+
+    def configure
+      self.configuration ||= Configuration.new
+      yield(configuration)
+    end
+  end
 end
--- lib/dev_toolbar/toolbar_helper.rb
+++ lib/dev_toolbar/toolbar_helper.rb
@@ -3,15 +3,11 @@ module DevToolbar
     def dev_toolbar
       return unless Rails.env.development?
 
-      content_tag(:div, id: 'dev-toolbar', style: 'position: fixed; right: 0; top: 0; background: #333; color: #fff; padding: 10px; z-index: 1000;') do
-        links = [
-          link_to('Routes', '/rails/info/routes', style: 'color: #fff; margin-right: 10px;'),
-          link_to('Database', '/rails/db', style: 'color: #fff; margin-right: 10px;'),
-          link_to('Web Console', '#', onclick: 'openWebConsole();', style: 'color: #fff; margin-right: 10px;')
-        ].join.html_safe
+      links = DevToolbar.configuration.links.map do |link|
+        link_to(link[:name], link[:path], style: 'color: #fff; margin-right: 10px;')
+      end.join.html_safe
 
-        safe_join([links])
-      end
+      content_tag(:div, links, id: 'dev-toolbar', style: 'position: fixed; right: 0; top: 0; background: #333; color: #fff; padding: 10px; z-index: 1000;')
     end
   end
 end
