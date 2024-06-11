37de9d5d949d64aaff45daa1b4c57b6334ad3b5c
Ben Purinton <ben.purinton@gmail.com>
2024-06-11 12:31:10 -0400

    Re-style the toolbar (#5)

--- lib/dev_toolbar/middleware.rb
+++ lib/dev_toolbar/middleware.rb
@@ -11,24 +11,61 @@ module DevToolbar
         response_body = response.body
         toolbar_html = <<-HTML
           <div id="dev-toolbar">
-            #{toolbar_links}
+            <div id="dev-toolbar-button">
+              <a id="dev-toolbar-toggle">🛠️</a>
+            </div>
+            <div id="dev-toolbar-links" class="hidden">
+              #{toolbar_links}
+            </div>
           </div>
           <style>
             #dev-toolbar {
               position: fixed;
               right: 0;
-              top: 0;
-              background: #333;
-              color: #fff;
-              padding: 0.5rem;
+              top: 50vh;
+              transform: translateY(-50%);
+              background-color: #f0f0f0;
+              border: 1px solid #ccc;
               z-index: 1000;
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica, Cantarell, Ubuntu, roboto, noto, arial, sans-serif;
+              color: #808080;
+            }
+        
+            #dev-toolbar-toggle {
+              font-size: 2em;
+              border: none;
+              cursor: pointer;
+              line-height: 1.5;
+              padding: 0 10px;
+              text-decoration: none;
+            }
+        
+            #dev-toolbar-links {
+              display: flex;
+              flex-direction: column;
             }
 
-            #dev-toolbar a {
-              color: #fff;
-              margin-right: 0.5rem;
+            .dev-toolbar-link {
+              padding: 5px 10px;
+              border-bottom: 1px #f0f0f0 solid;
+              color: #808080;
+              text-decoration: none;
+              background-color: white;
+            }
+        
+            #dev-toolbar-links.hidden {
+              display: none;
             }
           </style>
+          <script>
+            document.getElementById('dev-toolbar-toggle').addEventListener('click', function() {
+              var links = document.getElementById('dev-toolbar-links');
+              links.classList.toggle('hidden');
+            });
+          </script>
         HTML
 
         response_body.sub!('</body>', "#{toolbar_html}</body>")
@@ -44,7 +81,7 @@ module DevToolbar
 
     def toolbar_links
       DevToolbar.configuration.links.map do |link|
-        "<a href='#{link[:path]}' target='_blank'>#{link[:name]}</a>"
+        "<a href='#{link[:path]}' target='_blank' class='dev-toolbar-link'>#{link[:name]}</a>"
       end.join(' ')
     end
   end
