937c5c172c88eeb34e10747713eebc55c72a6e28
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2024-06-11 03:17:00 +0000

    Tweaks

--- lib/dev_toolbar/middleware.rb
+++ lib/dev_toolbar/middleware.rb
@@ -38,7 +38,7 @@ module DevToolbar
               font-size: 2em;
               border: none;
               cursor: pointer;
-              padding: 5px 10px;
+              padding: 10px;
             }
         
             #dev-toolbar-links {
