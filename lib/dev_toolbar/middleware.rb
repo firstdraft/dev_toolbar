afd93e5b2424c6ea1cc1d1ebeeb426dcdca143b4
Ben Purinton <ben.purinton@gmail.com>
2024-10-21 10:55:40 -0700

    Skip erd link if none present (#11)
    If there is no ERD, skip adding the link to it. There is a separate issue #6 where we want to perhaps find a better solution for hosting the ERD rather than in the `public/` folder, but for now, we could use a fix to prevent a 404 on the link if no ERD is present.
    
    We could just skip adding the `/erd.png` link in the initializer, but that requires us to remember and check that always. A better solution is to handle it in the gem in case we forget to remove it from the initializer. Then we can use a uniform initializer across all of our Rails projects and not need to have different configurations for different projects and have to keep them all up to date.

--- lib/dev_toolbar/middleware.rb
+++ lib/dev_toolbar/middleware.rb
@@ -81,8 +81,13 @@ module DevToolbar
 
     def toolbar_links
       DevToolbar.configuration.links.map do |link|
-        "<a href='#{link[:path]}' target='_blank' class='dev-toolbar-link'>#{link[:name]}</a>"
-      end.join(' ')
+        # if the erd.png file does not exist in /public, don't show the link
+        if link[:name] == "Data Model" && !File.exist?(Rails.public_path.join("erd.png"))
+          next
+        else
+          "<a href='#{link[:path]}' target='_blank' class='dev-toolbar-link'>#{link[:name]}</a>"
+        end
+      end.compact.join(" ")
     end
   end
 end
