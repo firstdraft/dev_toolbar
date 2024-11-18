2c0f8b191390f82d6d585c8039435ef95059d0e2
Ben Purinton <ben.purinton@gmail.com>
2024-11-18 13:23:35 -0800

    load controller

--- lib/dev_toolbar/engine.rb
+++ lib/dev_toolbar/engine.rb
@@ -1,6 +1,8 @@
 module DevToolbar
   class Engine < ::Rails::Engine
     isolate_namespace DevToolbar
+
+    config.autoload_paths << File.expand_path("../app/controllers", __FILE__)
     
     initializer "dev_toolbar.add_routes", after: :add_routing_paths do |app|
       app.routes.append do
