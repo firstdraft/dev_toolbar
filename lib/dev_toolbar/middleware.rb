abd6c168cc0df480fbd076b516955624735ea3c0
Ben Purinton <ben.purinton@gmail.com>
2024-11-18 10:39:06 -0800

    add copy

--- lib/dev_toolbar/middleware.rb
+++ lib/dev_toolbar/middleware.rb
@@ -95,7 +95,7 @@ module DevToolbar
                     Entity Relationship Diagram
                   </h1>
                   <p>
-                    To update this diagram after changes to your database or models (e.g. adding association accessors), open a terminal and run the command: <pre>rake erd</pre>
+                    To update this diagram after changes to your database or models (e.g. adding association accessors), open a terminal and run the command: <pre>rake erd</pre>, then close this page and reopen it from the toolbar link.
                   </p>
                   <div>
                     <img id="erd-image" src="/erd.png">
