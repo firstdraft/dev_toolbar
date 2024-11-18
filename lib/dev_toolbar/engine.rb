13074826488791fe5a0f7e1c16963d796d65d723
Ben Purinton <ben.purinton@gmail.com>
2024-11-18 13:18:41 -0800

    fix devise error

--- lib/dev_toolbar/engine.rb
+++ lib/dev_toolbar/engine.rb
@@ -2,14 +2,10 @@ module DevToolbar
   class Engine < ::Rails::Engine
     isolate_namespace DevToolbar
     
-    initializer "dev_toolbar.routes", after: :load_config_initializers do
-      Rails.application.routes.append do
-        mount DevToolbar::Engine, at: "/"
+    initializer "dev_toolbar.add_routes", after: :add_routing_paths do |app|
+      app.routes.append do
+        get "/erd", to: "dev_toolbar/erd#show"
       end
-    end
-
-    routes.draw do
-      get "/erd", to: "erd#show"
     end
   end
 end
