33491cc34531104cf8bcd51c7844c32cddbeb685
Ben Purinton <ben.purinton@gmail.com>
2024-11-18 13:25:55 -0800

    load views

--- lib/dev_toolbar/engine.rb
+++ lib/dev_toolbar/engine.rb
@@ -3,6 +3,7 @@ module DevToolbar
     isolate_namespace DevToolbar
 
     config.autoload_paths << File.expand_path("../app/controllers", __FILE__)
+    config.paths["app/views"] << File.expand_path("../app/views", __FILE__)
     
     initializer "dev_toolbar.add_routes", after: :add_routing_paths do |app|
       app.routes.append do
